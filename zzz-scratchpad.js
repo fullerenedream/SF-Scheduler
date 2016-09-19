876c64dfc2c494245147e128686f5e9edd8daa25
fullerenedream <fullerenedream@gmail.com>
2016-09-19 21:55:05 +0000

    changed technician_schedules db times from epoch time as INT to ISO 8600 as TIME

--- zzz-scratchpad.js
+++
@@ -1,6 +0,0 @@
-// with browser at localhost:3000, paste this into browser console
-// to see result of api call without having to navigate to
-// localhost:3000/api
-$.getJSON('/api',function(data){
-console.log(data);
-});
