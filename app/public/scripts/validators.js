73dec36b42ad50498b8017b0df3bb444dbd82a66
fullerenedream <fullerenedream@gmail.com>
2016-11-05 00:01:39 +0000

    updated GET for one resource and all its events - now color coding shows when an installer is selected from the Installers dropdown

---
+++ app/public/scripts/validators.js
@@ -0,0 +1,178 @@
+var moment = require('../fullcalendar-scheduler-1.4.0/lib/moment.min.js');
+
+module.exports = {
+
+  isBlankOrNull: function(val) {
+    if (val == '') {
+      console.log('val is blank');
+      return true;
+    }
+    else if (val == null) {
+      console.log('val is null');
+      return true;
+    }
+    else {
+      console.log('val is not blank or null. val: ' + val);
+      return false;
+    }
+  },
+
+  isInt: function(val) {
+    // check that input is a number
+    if (isNaN(val)) {
+      console.log(val + ' is not a number');
+      return false;
+    }
+    else if (val == null) {
+      console.log(val + ' is null');
+      return false;
+    }
+    // check that input is an integer
+    else if(val % 1 != 0){
+      console.log(val + ' is not an integer');
+      return false;
+    }
+    // All tests have passed, so return true
+    else {
+      console.log(val + ' is an integer');
+      return true;
+    }
+  },
+
+  isPositiveInt: function(val) {
+    if (isInt(val) == false) {
+      console.log(val + ' is a not an integer');
+    }
+    // check that input is greater than zero
+    else if (val <= 0) {
+      console.log(val + ' is a not a positive number');
+      return false;
+    }
+    // All tests have passed, so return true
+    else {
+      console.log(val + ' is a positive integer');
+      return true;
+    }
+  },
+
+  isValidDate: function(date) {
+    if ( moment(date, 'YYYY-MM-DD', true).isValid() == false ) {
+      console.log('invalid date: it is not of the form YYYY-MM-DD');
+      return false;
+    }
+    else return true;
+  },
+
+  isValidTime: function(time) {
+    if ( moment(time, 'HH:mm:ss', true).isValid() == false ) {
+      console.log('invalid time: it is not of the form HH:mm:ss');
+      return false;
+    }
+    else return true;
+  },
+
+  isValidISO8601: function(ISO8601) {
+    if ( moment(ISO8601, 'YYYY-MM-DDTHH:mm:ss', true).isValid() == false ) {
+      console.log(ISO8601 + ' is invalid ISO8601: it is not of the form YYYY-MM-DDTHH:mm:ss');
+      return false;
+    }
+    else {
+    console.log(ISO8601 + ' is valid ISO8601');
+    return true;
+    }
+  },
+
+  hasDateAndStartTime: function(appointment) {
+    if ( isValidDate(appointment[3]) && isValidTime(appointment[4]) ) {
+      console.log('appointment has valid date and start time');
+      return true;
+    }
+    else if ( isValidISO8601(appointment[6]) ){
+      console.log('appointment has valid ISO8601 start time');
+      return true;
+    }
+    else {
+      console.log('appointment is missing date and/or start time');
+      return false;
+    }
+  },
+
+  hasDateStartAndEndTime: function(appointment) {
+    if ( isValidDate(appointment[3]) && isValidTime(appointment[4]) && isValidTime(appointment[5])) {
+      console.log('appointment has valid date, start and end times');
+      return true;
+    }
+    else if ( isValidISO8601(appointment[6]) && isValidISO8601(appointment[7]) ){
+      console.log('appointment has valid ISO8601 start and end times');
+      return true;
+    }
+    else {
+      console.log('appointment is missing date, start or end time');
+      return false;
+    }
+  },
+
+  // WARNING: tech_id's are temporarily hard-coded in this function
+  // TODO: make function check to see if this tech_id exists in db
+  isValidTechId: function(tech_id) {
+    if (tech_id == 1 || tech_id == 2 || tech_id == 3) {
+      console.log('valid tech_id: ' + tech_id);
+      return true;
+    }
+    else {
+      console.log('invalid tech_id: ' + tech_id);
+      return false;
+    }
+  },
+
+  // WARNING: appointment_type values are temporarily hard-coded in this function
+  // TODO: make function check to see if this appointment_type exists in db
+  isValidAppointmentType: function(appointment_type) {
+    if ( (appointment_type >= 1 && appointment_type <= 5) || appointment_type == 8 || appointment_type == 10 ) {
+      console.log('appointment_type is valid');
+      return true;
+    }
+    else {
+      console.log('appointment_type is invalid');
+      return false;
+    }
+  },
+
+  // checks appointment_type to see if it is Time Off (=10)
+  isTimeOff: function(appointment) {
+    if (appointment[0] == '10') {
+      console.log(appointment[0]);
+      console.log('appointment is Time Off');
+    }
+    else {
+      console.log(appointment[0]);
+      console.log('appointment is not Time Off');
+    }
+    return appointment[0] == '10' ? true : false;
+  },
+
+  // if appointment is not time off, and date or start time are blank or null, appointment is an On Deck item
+  isOnDeck: function(appointment) {
+    if (  (!isTimeOff(appointment)) && ( isBlankOrNull(appointment[3]) || isBlankOrNull(appointment[4]) )  ) {
+      console.log('appointment is On Deck');
+      return true;
+    }
+    else {
+      console.log('appointment is not On Deck');
+      return false;
+    }
+  },
+
+  // if appointment has no tech_id, is not Time off, has date and time -> appointment is Unassigned
+  isUnassigned: function(appointment) {
+    if ( isBlankOrNull(appointment[2]) && (!isTimeOff(appointment)) &&  isValidDate(appointment[3]) && isValidTime(appointment[4]) && isValidTime(appointment[5]) ) {
+      console.log('appointment is Unassigned');
+      return true;
+    }
+    else {
+      console.log('appointment is not Unassigned');
+      return false;
+    }
+  }
+
+};
