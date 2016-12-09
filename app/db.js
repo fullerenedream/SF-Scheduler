7b3c4b466d801668e624046b8a978c6a1de1ed83
fullerenedream <fullerenedream@gmail.com>
2016-12-09 00:02:20 +0000

    use env variables for db connection string

--- app/db.js
+++ app/db.js
@@ -6,7 +6,10 @@ module.exports = {
   // in NeoNodoSchedule - need to end the connection,
   // but how to organize it?
   connectToScheduleDB: function() {
-    var con = mysql.createConnection({
+
+    var conn_string = process.env.JAWSDB_URL; //Get the connection string
+
+    var con = mysql.createConnection(conn_string || {
       host      : 'localhost',
       user      : 'swiftfox',
       password  : 'swiftfox',
